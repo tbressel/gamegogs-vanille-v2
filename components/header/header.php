dcf3f8fde90080c54f9a2563d3874c072aa660f8
tbressel <tbressel.dev@gmail.com>
2023-09-28 16:38:47 +0200

    html and css part of mobile navigation done

--- components/header/header.php
+++ components/header/header.php
@@ -1,38 +1,55 @@
-echo ('<div class="header__container black-bg">
-            <div class="header__container--top">
-                <div class="header__container--brandname">
+echo (' <div class="header__container black-bg">
+    <div class="header__container--top">
+        <div class="header__container--brandname">
 
-                    <div class="brandname__top">
-                        <div class="brandname__top--joystick">
-                            <img src="./assets/svg/joystick.svg" alt="joystick icon">
-                        </div>
-                        <div class="brandname__top--title">
-                            <img src="./assets/svg/gamegog-logo.svg" alt="gamegog logo">
-                        </div>
-                    </div>
-
-                    <div class="brandname__bot">
-                        <div class="brandname__bot--hook">
-                            <img src="./assets/svg/hook.svg" alt="la chasse aux tresors">
-                        </div>
-                    </div>
-
+            <div class="brandname__top">
+                <div class="brandname__top--joystick">
+                    <img src="./assets/svg/joystick.svg" alt="joystick icon">
                 </div>
-                <div class="header__container--burger">
-                    <img src="./assets/svg/burger-icon.svg" alt="burger icon">
+                <div class="brandname__top--title">
+                    <img src="./assets/svg/gamegog-logo.svg" alt="gamegog logo">
                 </div>
             </div>
-            <div class="header__container--bot">
-                <div class="header__container--searchfield">
-                    <form action="#">
-                        <label for="search">
-                            <input class="field-search white-bg" id="search" type="search" placeholder="Recherche GameGogs">
-                        </label>
-                    </form>
-                    
-                </div>
-                <div class="header__container--zoom">
-                    <img src="./assets/svg/zoom-icon.svg" alt="search on gamegogs">
+
+            <div class="brandname__bot">
+                <div class="brandname__bot--hook">
+                    <img src="./assets/svg/hook.svg" alt="la chasse aux tresors">
                 </div>
             </div>
-        </div>');
+
+        </div>
+        <div class="header__container--burger">
+            <img src="./assets/svg/burger-icon.svg" alt="burger icon">
+        </div>
+    </div>
+    <div class="header__container--bot">
+        <div class="header__container--searchfield">
+            <form action="#">
+                <label for="search">
+                    <input class="field-search white-bg" id="search" type="search" placeholder="Recherche GameGogs">
+                </label>
+            </form>
+
+        </div>
+        <div class="header__container--zoom">
+            <img src="./assets/svg/zoom-icon.svg" alt="search on gamegogs">
+        </div>
+    </div>
+    <div class="header__container--menu">
+        <ul class="menu__ul">
+            <li class="menu__icon dashboard">
+                <img src="./assets/svg/dashboard-icon.svg" alt="dashboard">
+            </li>
+            <li class="menu__icon mail">
+                <img src="./assets/svg/mail-icon.svg" alt="mail">
+            </li>
+            <li class="menu__icon cart">
+                <img src="./assets/svg/cart-icon.svg" alt="cart">
+            </li>
+            <li class="menu__icon profilarrow">
+                <img src="./assets/svg/profil_icon.svg" alt="profil">
+                <img src="./assets/svg/arrowprofil-icon.svg" alt="arrow">
+            </li>
+        </ul>
+    </div>
+</div>');
