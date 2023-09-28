91e136ccbc35d2af3dfd6b56bad173c5916bd7a3
tbressel <tbressel.dev@gmail.com>
2023-09-28 22:46:49 +0200

    display menu and submenu in mobile navigation done

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,4 +1,45 @@
-// Listener on the burger button and active class state
-document.getElementById('burger-btn').addEventListener('click', (event) => {
+// ---------------------------------------------------------------
+// -------------------- global declaration -----------------------
+// ---------------------------------------------------------------
+let BurgerMenuState = false;
+let SubMenuState = false;
+
+
+
+// ---------------------------------------------------------------
+// ---------------------- functions declaration ------------------
+// ---------------------------------------------------------------
+function toggleBurgerMenu(event) {
     document.querySelector(`.navigation__container .${event.target.getAttribute('data-set')}`).classList.toggle('active');
+    BurgerMenuState = !BurgerMenuState;
+}
+
+function toggleSubMenu(event) {
+    document.querySelector(`.navigation__container .${event.target.getAttribute('data-set')}`).classList.toggle('active');
+    SubMenuState = !SubMenuState;
+}
+
+function closeSubMenu() {
+    document.querySelector(`.navigation__container .menu__ul--profil`).classList.toggle('active');
+    SubMenuState = !SubMenuState; 
+}
+
+
+
+
+
+// ---------------------------------------------------------------
+// ------ Listener on the burger button and active class state ---
+// ---------------------------------------------------------------
+document.getElementById('burger-btn').addEventListener('click', (event) => {
+    toggleBurgerMenu(event);
+    // test if sub menu is active, if it is, we close it
+    if (SubMenuState) {
+        closeSubMenu()
+    }
+
 })
+document.getElementById('profilarrow-btn').addEventListener('click', (event) => {
+    toggleSubMenu(event);
+})
+
