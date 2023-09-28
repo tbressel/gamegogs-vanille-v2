5cdeb07cfa9cde0b44ae168da2f40f3184d44e69
tbressel <tbressel.dev@gmail.com>
2023-09-28 23:32:47 +0200

    rotate submenu arrow

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -17,6 +17,7 @@ function toggleBurgerMenu(event) {
 function toggleSubMenu(event) {
     document.querySelector(`.navigation__container .${event.target.getAttribute('data-set')}`).classList.toggle('active');
     SubMenuState = !SubMenuState;
+    reverseProfilArrow();
 }
 
 function closeSubMenu() {
@@ -24,7 +25,9 @@ function closeSubMenu() {
     SubMenuState = !SubMenuState; 
 }
 
-
+function reverseProfilArrow() {
+    document.getElementById('arrow-mobile').classList.toggle('down');
+}
 
 
 
@@ -35,11 +38,12 @@ document.getElementById('burger-btn').addEventListener('click', (event) => {
     toggleBurgerMenu(event);
     // test if sub menu is active, if it is, we close it
     if (SubMenuState) {
-        closeSubMenu()
+        closeSubMenu();
+        reverseProfilArrow();
     }
 
 })
-document.getElementById('profilarrow-btn').addEventListener('click', (event) => {
+document.getElementById('profilarrow-btn-mobile').addEventListener('click', (event) => {
     toggleSubMenu(event);
 })
 
