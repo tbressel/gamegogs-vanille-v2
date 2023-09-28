42b18024a2a30f233d9258673f8353c87023194f
tbressel <tbressel.dev@gmail.com>
2023-09-28 10:23:41 +0200

    first deposit

---
+++ components/header/header.php
@@ -0,0 +1,38 @@
+echo ('<div class="header__container black-bg">
+            <div class="header__container--top">
+                <div class="header__container--brandname">
+
+                    <div class="brandname__top">
+                        <div class="brandname__top--joystick">
+                            <img src="./assets/svg/joystick.svg" alt="joystick icon">
+                        </div>
+                        <div class="brandname__top--title">
+                            <img src="./assets/svg/gamegog-logo.svg" alt="gamegog logo">
+                        </div>
+                    </div>
+
+                    <div class="brandname__bot">
+                        <div class="brandname__bot--hook">
+                            <img src="./assets/svg/hook.svg" alt="la chasse aux tresors">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="header__container--burger">
+                    <img src="./assets/svg/burger-icon.svg" alt="burger icon">
+                </div>
+            </div>
+            <div class="header__container--bot">
+                <div class="header__container--searchfield">
+                    <form action="#">
+                        <label for="search">
+                            <input class="field-search white-bg" id="search" type="search" placeholder="Recherche GameGogs">
+                        </label>
+                    </form>
+                    
+                </div>
+                <div class="header__container--zoom">
+                    <img src="./assets/svg/zoom-icon.svg" alt="search on gamegogs">
+                </div>
+            </div>
+        </div>');
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GameGogs - La chasse aux trésors</title>
+    <link rel="stylesheet" href="./assets/style/css/index.css">
+</head>
+
+<body>
+    <header class="black-bg">
+        <?php include('./components/header/header.php'); ?>
+    </header>
+
+
+
+    <footer>
+    
+    </footer>
+
+
+</body>
+
+</html>
