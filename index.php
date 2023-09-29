71eaa0e1f421a0e2f72d530766a91bb3b6cdfef5
tbressel <tbressel.dev@gmail.com>
2023-09-29 14:05:29 +0200

    social networks icon and responsive mobile tablet and desktop is all done

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameGogs - La chasse aux trésors</title>
     <meta name="description" content="Base de données pour les collectionneurs de jeux vidéos rétro et nextgen. Gestion de contenu détaillé et marketplace ou bien simple gestionnaire de collection">
+    
+    <link rel="icon" href="./favicon.ico">
     <link rel="stylesheet" href="./assets/style/css/index.css">
 </head>
 
@@ -27,35 +29,9 @@
 
     <footer class="black-bg">
         <div class="footer__container">
-
-            <div class="socialnetworks__container">
-                <div class="socialnetwork__block">
-                    <div class="socialnetwork__title">
-                        <p>Suivez-nous</p>
-                    </div>
-                    <ul id="follow__ul" class="socialnetworkmenu__follow">
-                        <li class="socialnetworkmenu__li">
-                            <img src="./assets/svg/facebook-logo.svg" alt="facebook">
-                            <p>Aide & Support</p>
-                        </li>
-                        <li class="socialnetworkmenu__li">
-                            <p>Laisser une évaluation</p>
-                        </li>
-                        <li class="socialnetworkmenu__li">
-                            <p>Raccourcis clavier</p>
-                        </li>
-                        <li class="socialnetworkmenu__li">
-                            <p>Guide d\'utilisation de la base de données</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-
-
-
-
+            <?php include ('./components/footer/social-networks/social-networks.php'); ?>
             <?php include('./components/footer/about/about.php'); ?>
+            <?php include('./components/footer/rgpd/rgpd.php'); ?>
         </div>
     </footer>
     <script src="./assets/scripts/script.js"></script>
